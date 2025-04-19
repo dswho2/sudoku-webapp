12c9e271ed24facfbb8e517f9e52a8cfb64bd55b
David Ho <davidsw.ho02@gmail.com>
2025-04-19 12:31:49 -0700

    added clarifying comments

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,7 @@
 import os
 
 class Config:
+    # in vercel backend env var settings for prod
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
