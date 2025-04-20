4ffb5da4a43c961c0b6caf059c05521855c79a99
David Ho <davidsw.ho02@gmail.com>
2025-04-19 17:09:01 -0700

    changed to vercel neon postgres storage

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,8 +2,15 @@
 
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL", "")
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
 class Config:
     # in vercel backend env var settings for prod
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
